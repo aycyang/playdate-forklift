efa0f7fc9deb9e7e8f195004b741789d2920dca4
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-09-06 20:06:35 -0400

    Refactor forklift into class

--- source/main.lua
+++ source/main.lua
@@ -1,17 +1,54 @@
 import "CoreLibs/graphics"
 
 function init()
-  forkliftHeight = 20
-  forkliftX = 300
-  forkliftY = 200
-  fork = makeAABB(0, 0, 0, 0)
+  forklift = newForklift(300, 200)
+  fork = newAABB(0, 0, 0, 0)
   ground = makeGround(200)
   packages = {}
   table.insert(packages, makePackage(120, 100))
   table.insert(packages, makePackage(250, 100))
 end
 
-function makeAABB(x, y, w, h)
+function newForklift(x, y)
+  local widthBase <const> = 50
+  local widthCockpit <const> = 30
+  local heightBase <const> = 20
+  local heightCockpit <const> = 50
+  local forkWidth <const> = 2
+  local forkMaxHeight <const> = 120
+  local forkLength <const> = 30
+  local forkGap <const> = 1
+  local obj = {
+    x = x,
+    y = y,
+    forkHeight = 20,
+  }
+  function obj:raiseFork(crankChange)
+    self.forkHeight = clamp(self.forkHeight + crankChange, 0, 120)
+  end
+  function obj:move(dx)
+    self.x += dx
+  end
+  function obj:getForkAABB()
+    return newAABB(
+      self.x - widthBase/2 - forkWidth - forkGap - forkLength,
+      self.y - self.forkHeight,
+      forkLength,
+      forkWidth)
+  end
+  function obj:draw(gfx)
+    gfx.fillRect(self.x - widthBase/2, self.y - heightBase, widthBase, heightBase)
+    gfx.drawRect(self.x - widthCockpit/2, self.y - heightCockpit, widthCockpit, heightCockpit)
+    gfx.fillRect(self.x - widthBase/2 - forkWidth - forkGap, self.y - forkMaxHeight, forkWidth, forkMaxHeight)
+    -- fork
+    gfx.fillRect(self.x - widthBase/2 - forkWidth - forkGap - forkLength, self.y - self.forkHeight, forkLength, forkWidth)
+  end
+  function obj:update(dt)
+  end
+  return obj
+end
+
+function newAABB(x, y, w, h)
   return {
     x = x,
     y = y,
@@ -72,42 +109,25 @@ end
 
 -- (x, y) refers to the bottom center of the forklift
 function drawForklift(gfx, x, y, h)
-  local widthBase = 40
-  local widthCockpit = 30
-  local heightBase = 20
-  local heightCockpit = 50
-  local forkWidth = 2
-  local forkMaxHeight = 120
-  local forkLength = 30
-  local forkGap = 1
-  local forkHeight = h
-  gfx.fillRect(x - widthBase/2, y - heightBase, widthBase, heightBase)
-  gfx.drawRect(x - widthCockpit/2, y - heightCockpit, widthCockpit, heightCockpit)
-  gfx.fillRect(x - widthBase/2 - forkWidth - forkGap, y - forkMaxHeight, forkWidth, forkMaxHeight)
-  -- fork
-  gfx.fillRect(x - widthBase/2 - forkWidth - forkGap - forkLength, y - forkHeight, forkLength, forkWidth)
 end
 
 function playdate.update()
   -- handle input
   local change, _ = playdate.getCrankChange()
-  forkliftHeight = clamp(forkliftHeight + change, 0, 120)
+  forklift:raiseFork(change)
 
   if playdate.buttonIsPressed( playdate.kButtonRight ) then
-    forkliftX += 2
+    forklift:move(2)
   end
   if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-    forkliftX -= 2
+    forklift:move(-2)
   end
 
   -- update
   local deltaTime = playdate.getElapsedTime()
   playdate.resetElapsedTime()
   -- update forklift fork collider
-  fork.x = forkliftX - 53
-  fork.y = forkliftY - forkliftHeight
-  fork.w = 30
-  fork.h = 2
+  fork = forklift:getForkAABB()
   -- gravity and try update position
   for i = 1, #packages do
     packages[i]:update(deltaTime)
@@ -138,7 +158,7 @@ function playdate.update()
   -- draw
   playdate.graphics.clear()
   ground:draw(playdate.graphics)
-  drawForklift(playdate.graphics, forkliftX, forkliftY, forkliftHeight)
+  forklift:draw(playdate.graphics)
   for i = 1, #packages do
     packages[i]:draw(playdate.graphics)
   end

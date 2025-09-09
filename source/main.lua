47308a9744266a696f4979bfbffcbe7aa85192eb
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-09-09 15:31:28 -0400

    Implement Y direction; fix some bugs around pushing in the minus direction

--- source/main.lua
+++ source/main.lua
@@ -10,13 +10,9 @@ import "CoreLibs/object"
 import "CoreLibs/sprites"
 
 -- TODO
--- * tryMove(x, y): check for collisions along each axis (x, y) individually.
---   for all bodies returned, recursively call tryMove on them, with the current
---   distance subtracted. once all tryMove calls have returned, call
---   self:moveWithCollisions(x, y).
+-- * gravity
+-- * static friction
 -- * use delta time instead of fixed framerate
--- * make blocks stack
--- * make the block move left and right
 -- * turn the forklift
 
 -- ### Collision system rules:
@@ -41,6 +37,12 @@ local TAGS <const> = {
   static = 1,
   dynamic = 2,
 }
+
+function warnIfNot(cond, msg)
+  if not cond then
+    error("warning: " .. msg)
+  end
+end
 
 function sign(n)
   if n < 0 then
@@ -99,6 +101,47 @@ function Body:init(x, y, w, h, tag)
 end
 
 -- Without moving, return the maximum distance this body can travel along the
+-- y-axis, up to goalY. Returns a value 0 through goalY.
+function Body:checkMoveByY(goalY, recursionDepth)
+  -- If this is a recursive call on a StaticBody, it should not move.
+  if recursionDepth > 0 and self:getTag() == TAGS.static then return 0 end
+  -- Recursively call checkMoveBy on any Body that is in the way.
+  -- Adjust the return value accordingly.
+  local _, _, collisions, numCollisions = self:checkCollisions(self.x, self.y + goalY)
+  local minY = math.huge
+  for i = 1, numCollisions do
+    local other <const> = collisions[i].other
+    local dist <const> = self:distY(other)
+    warnIfNot(dist >= 0, "x distance was negative; maybe a body clipped inside another body?")
+    local signedDist <const> = sign(goalY) * dist
+    local truncY <const> = signedDist + other:checkMoveByY(goalY - signedDist, recursionDepth + 1)
+    if math.abs(truncY) < math.abs(minY) then
+      minY = truncY
+    end
+  end
+  if minY < math.huge then
+    return minY
+  end
+  return goalY
+end
+
+function Body:tryMoveByY(goalY, verbose, recursionDepth)
+  recursionDepth = recursionDepth or 0
+  -- Constrain movement based on how far subsequent bodies can be moved.
+  local actualY = self:checkMoveByY(goalY, recursionDepth)
+  local _, _, collisions, numCollisions = self:checkCollisions(self.x, self.y + actualY)
+  for i = 1, numCollisions do
+    local other <const> = collisions[i].other
+    local dist <const> = self:distY(other)
+    warnIfNot(dist >= 0, "x distance was negative; maybe a body clipped inside another body?")
+    local signedDist <const> = sign(goalY) * dist
+    other:tryMoveByY(actualY - signedDist, recursionDepth + 1)
+  end
+  if verbose and actualY ~= goalY then print("goalY="..goalY..", actualY="..actualY) end
+  self:moveBy(0, actualY)
+end
+
+-- Without moving, return the maximum distance this body can travel along the
 -- x-axis, up to goalX. Returns a value 0 through goalX.
 function Body:checkMoveByX(goalX, recursionDepth)
   -- If this is a recursive call on a StaticBody, it should not move.
@@ -110,17 +153,20 @@ function Body:checkMoveByX(goalX, recursionDepth)
   for i = 1, numCollisions do
     local other <const> = collisions[i].other
     local dist <const> = self:distX(other)
-    assert(dist >= 0)
-    local truncX <const> = dist + other:checkMoveByX(goalX - dist, recursionDepth + 1)
-    minX = math.min(minX, truncX)
+    warnIfNot(dist >= 0, "x distance was negative; maybe a body clipped inside another body?")
+    local signedDist <const> = sign(goalX) * dist
+    local truncX <const> = signedDist + other:checkMoveByX(goalX - signedDist, recursionDepth + 1)
+    if math.abs(truncX) < math.abs(minX) then
+      minX = truncX
+    end
   end
-  if minX < math.huge then
+  if minX ~= math.huge then
     return minX
   end
   return goalX
 end
 
-function Body:tryMoveByX(goalX, recursionDepth)
+function Body:tryMoveByX(goalX, verbose, recursionDepth)
   recursionDepth = recursionDepth or 0
   -- Constrain movement based on how far subsequent bodies can be moved.
   local actualX = self:checkMoveByX(goalX, recursionDepth)
@@ -128,9 +174,11 @@ function Body:tryMoveByX(goalX, recursionDepth)
   for i = 1, numCollisions do
     local other <const> = collisions[i].other
     local dist <const> = self:distX(other)
-    assert(dist >= 0)
-    other:tryMoveByX(actualX - dist, recursionDepth + 1)
+    warnIfNot(dist >= 0, "x distance was negative; maybe a body clipped inside another body?")
+    local signedDist <const> = sign(goalX) * dist
+    other:tryMoveByX(actualX - signedDist, recursionDepth + 1)
   end
+  if verbose and actualX ~= goalX then print("goalX="..goalX..", actualX="..actualX) end
   self:moveBy(actualX, 0)
 end
 
@@ -154,7 +202,7 @@ function DynamicBody:draw(x, y, w, h)
   gfx.drawRect(x, y, w, h)
 end
 
-local fork = StaticBody(100, 100, 30, 5)
+local fork = StaticBody(100, 100, 30, 20)
 local ground = StaticBody(50, 50, 60, 40)
 local bodyA = StaticBody(200, 80, 10, 40)
 local bodyB = StaticBody(300, 120, 30, 70)
@@ -174,6 +222,9 @@ local bodyK = StaticBody(200, 40, 20, 20)
 local bodyL = DynamicBody(160, 40, 20, 20)
 local bodyM = DynamicBody(130, 40, 20, 20)
 local bodyN = DynamicBody(100, 40, 20, 20)
+local bodyO = DynamicBody(140, 80, 20, 20)
+local bodyP = StaticBody(50, 110, 20, 20)
+local bodyQ = DynamicBody(50, 85, 20, 20)
 bodyI:tryMoveByX(20)
 
 function init()
@@ -198,7 +249,7 @@ function playdate.update()
     local change, _ = playdate.getCrankChange()
     dy = -change
   end
-  fork:moveWithCollisions(fork.x, fork.y + dy)
+  fork:tryMoveByY(dy)
 
   -- handle player x-axis movement
   if playdate.buttonIsPressed(playdate.kButtonRight) then
@@ -207,12 +258,13 @@ function playdate.update()
   if playdate.buttonIsPressed(playdate.kButtonLeft) then
     dx = -2
   end
-  fork:moveWithCollisions(fork.x + dx, fork.y)
+  fork:tryMoveByX(dx)
 
   -- kinematic bodies
   bodyA:tryMoveByX(3)
   bodyD:tryMoveByX(7)
   bodyK:tryMoveByX(-2)
+  bodyP:tryMoveByY(-2)
 
   -- draw
   --gfx.clear()
